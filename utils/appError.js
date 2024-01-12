15b39716523d40d051f35210273b9bca320b8567
Ala Ben Aissia <xalabenaissiax@outlook.com>
2024-01-12 14:15:09 +0100

    Dev Vs Prod error handling

--- utils/appError.js
+++ utils/appError.js
@@ -1,11 +1,11 @@
 class AppError extends Error {
 	constructor(message, statusCode) {
 		super(message);
-		this.message = message;
-		this.statusCode = statusCode;
+		// this.errorMessage = message;
 		this.status = `${statusCode}`.startsWith("4")
 			? "fail"
 			: "error";
+		this.statusCode = statusCode;
 		this.isOperational = true;
 		Error.captureStackTrace(this, this.constructor); // Without this line of code, the error stack would include the internal Error.captureStackTrace function call. However, with this implementation, the error stack starts from the point where the AppError instance is created, providing a more accurate representation of the function calls that led to the error
 	}
--- utils/globalErrorHandler.js
+++ utils/globalErrorHandler.js
@@ -1,4 +1,6 @@
-const globalErrorHandler = (err, req, res, next) => {
+const AppError = require("./AppError");
+
+const handleErrorsDev = (err, res) => {
 	return res.status(err.statusCode ?? 500).json({
 		customError: {
 			status: err.status ?? "error",
@@ -9,6 +11,42 @@ const globalErrorHandler = (err, req, res, next) => {
 			// stack: err.stack,
 		},
 	});
+};
+
+const handleErrorsProd = (err, res) => {
+	if (err.isOperational) {
+		return res.status(err.statusCode).json({
+			message: err.message,
+			err,
+		});
+	} else {
+		return res.status(500).json({
+			status: "error",
+			message: "OoOps.. Something went wrong!",
+		});
+	}
+};
+
+const handleCastError = (err) =>
+	new AppError(`Invalid ${err.path}: ${err.value}`, 400);
+
+const handleDuplicateKeyError = (err) => {
+	console.log(err.message);
+	const [[field, value]] = Object.entries(err.keyValue);
+	const msg = `duplicate key error collection: ${field} '${value}' already exists`;
+	return new AppError(msg, 400);
+};
+
+const globalErrorHandler = (err, req, res, next) => {
+	if (process.env.NODE_ENV === "development") {
+		handleErrorsDev(err, res);
+	}
+	if (process.env.NODE_ENV === "production") {
+		let error = { ...err };
+		if (err.name === "CastError") error = handleCastError(err);
+		if (err.code === 11000) error = handleDuplicateKeyError(err);
+		handleErrorsProd(error, res);
+	}
 };
 
 module.exports = globalErrorHandler;
