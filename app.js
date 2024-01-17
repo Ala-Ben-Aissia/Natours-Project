5391cbddf6cfa52b59d77f3a51e28170d54ca7c9
Ala Ben Aissia <xalabenaissiax@outlook.com>
2024-01-17 16:11:39 +0100

    auth sign up and login with jwt

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const { toursRouter } = require("./routes/tourRoutes");
 const globalErrorHandler = require("./utils/globalErrorHandler");
 const morgan = require("morgan");
 const userRouter = require("./routes/userRoutes");
+const authRouter = require("./routes/authRoutes");
 
 const app = express();
 
@@ -15,6 +16,7 @@ app.use(morgan("dev"));
 // Tours middleware
 app.use("/tours", toursRouter);
 app.use("/users", userRouter);
+app.use("/auth", authRouter);
 
 // Global Error Handling
 app.use(globalErrorHandler);
