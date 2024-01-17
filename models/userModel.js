7361165aafadbc77aaff6e97067f6b8b1d2237ac
Ala Ben Aissia <xalabenaissiax@outlook.com>
2024-01-17 16:11:51 +0100

    auth sign up and login with jwt

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
 const userSchema = new mongoose.Schema({
 	username: {
 		type: String,
@@ -51,6 +52,13 @@ const userSchema = new mongoose.Schema({
 		default: Date.now(),
 		// select: false,
 	},
+});
+
+userSchema.pre("save", async function (next) {
+	if (!this.isModified("password")) return next();
+	this.password = await bcrypt.hash(this.password, 12);
+	this.passwordConfirm = undefined; // useful only when registration
+	next();
 });
 
 userSchema.pre(/^find/, function (next) {
