8ba6d054a349dd395df435cb28c72ac06751317e
Ala Ben Aissia <xalabenaissiax@outlook.com>
2024-01-23 13:26:28 +0100

    security and vulnerabilities handler middlewares

--- app.js
+++ app.js
@@ -6,11 +6,43 @@ const userRouter = require("./routes/userRoutes");
 const authRouter = require("./routes/authRoutes");
 const reviewRouter = require("./routes/reviewRoutes");
 const AppError = require("./utils/appError");
+const { default: rateLimit } = require("express-rate-limit");
+const { default: helmet } = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const hpp = require("hpp");
 
 const app = express();
 
 // Body Parser
 app.use(express.json());
+
+// helmet (setting HTTP response headers)
+app.use(helmet());
+
+// data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// prevent params pollution
+// app.use(
+// 	hpp({
+// 		whitelist: [
+// 			"duration",
+// 			"ratingsQuantity",
+// 			"ratingsAverage",
+// 			"difficulty",
+// 			"price",
+// 		],
+// 		// fileds that are allowed to be used multiple times in querying(params)
+// 	})
+// );
+
+// limit requests rate(throttling)
+const limiter = rateLimit({
+	limit: 100, // default to 5
+	windowMs: 60 * 60 * 1000, // 100 requests per hour
+	message: `Too many requests, please try again after 1 hour !`,
+});
+app.use("/api", limiter);
 
 // Logger middleware
 app.use(morgan("dev"));
