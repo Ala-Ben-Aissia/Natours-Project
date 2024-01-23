dadd574d2c515e73372337113d07bbd1f34ee0a1
Ala Ben Aissia <xalabenaissiax@outlook.com>
2024-01-23 20:59:00 +0100

    preparing pug skeleton

--- app.js
+++ app.js
@@ -63,6 +63,18 @@ app.get("/", (req, res) => {
 	});
 });
 
+app.get("/overview", (req, res) => {
+	res.status(200).render("overview", {
+		title: "All Tours",
+	});
+});
+
+app.get("/tour", (req, res) => {
+	res.status(200).render("tour", {
+		title: "The Forest Hiker Tour",
+	});
+});
+
 app.use("/api/v1/tours", toursRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/auth", authRouter);
