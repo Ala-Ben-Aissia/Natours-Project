87b993f41d6a8dd5ec1142547ab9d70b02079e37
Ala Ben Aissia <xalabenaissiax@outlook.com>
2024-01-25 10:50:44 +0100

    improve tour stops markers & popups

--- public/js/leaflet.js
+++ public/js/leaflet.js
@@ -3,23 +3,36 @@ const locations = JSON.parse(
    document.getElementById("map").dataset.locations
 );
 document.addEventListener("DOMContentLoaded", function () {
-   const mymap = L.map("map").setView([105, 42], 5);
-   L.polygon(locations.map((e) => e.coordinates)).addTo(mymap);
+   const mymap = L.map("map").setView(locations[0].coordinates, 10);
    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
       attribution: "© OpenStreetMap contributors",
    }).addTo(mymap);
+   // L.circle([lat, lng], {
+   //    color: "red",
+   //    fillColor: "#f03",
+   //    fillOpacity: 0.5,
+   //    radius: 5000,
+   // }).addTo(mymap);
+   // L.polygon();
+   L.polygon(locations.map((e) => e.coordinates.reverse())).addTo(
+      mymap
+   );
    locations.forEach((loc) => {
-      const [lng, lat] = loc.coordinates;
+      const [lng, lat] = loc.coordinates.reverse();
       L.marker([lat, lng])
          .addTo(mymap)
-         .bindPopup(L.popup())
-         .setPopupContent(loc.description)
+         .bindPopup(
+            L.popup({
+               maxWidth: 250,
+               minWidth: 100,
+               autoClose: false,
+               closeOnClick: false,
+               // className: "mapboxgl-popup",
+            })
+         )
+         .setPopupContent(
+            `<h2 class="mapboxgl-popup-content">${loc.description}</h2>`
+         )
          .openPopup();
-      // L.circle([lat, lng], {
-      //    color: "red",
-      //    fillColor: "#f03",
-      //    fillOpacity: 0.5,
-      //    radius: 5000,
-      // }).addTo(mymap);
    });
 });
